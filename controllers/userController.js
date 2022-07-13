68d390fb5ccaa0d424f01a6f704d300c4ef864b0
Lara Sanchez <lara3795@hotmail.com>
2022-07-13 20:36:21 +0100

    Auth and encrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
-const UserModel = require("../models/userModel");
+const UserModel = require("../models/userModel")
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 
 async function createUser(req, res) {
   try {
+    const hash = bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hash
+
     const user = await UserModel.create(req.body);
-    res.json(user);
+
+    const payload = {email: user.email}
+    const token = jwt.sign(payload, process.env.SECRET, {expiresIn: "1h"})
+
+    res.status(200).json({email: user.email, token: token})
+    
   } catch (error) {
     console.log(error);
   }
