745cf7bed0777c67a0f67c219b83c6c4f32096cf
Jonathan Holz Gamero <jonholzg@gmail.com>
2022-07-18 13:40:27 -0500

    room and purchase

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,14 @@ const router = require("express").Router();
 
 const userRouter = require("./userRouter");
 const roomRouter = require("./roomRouter");
+const employeeRouter = require("./employeeRouter");
+const movieRouter = require("./movieRouter");
+const purchaseRouter = require("./purchaseRouter");
 
 router.use("/user", userRouter);
 router.use("/room", roomRouter);
-const movieRouter = require("./movieRouter");
-
-router.use("/user", userRouter);
 router.use("/movie", movieRouter);
-const employeeRouter = require("./employeeRouter");
-
-router.use("/user", userRouter);
 router.use("/employee", employeeRouter);
+router.use("/purchase", purchaseRouter);
 
 module.exports = router;
